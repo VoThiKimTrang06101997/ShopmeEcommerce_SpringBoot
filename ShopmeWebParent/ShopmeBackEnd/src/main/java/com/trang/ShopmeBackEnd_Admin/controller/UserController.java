bd3f202e3c44e763a87912f77d3005e4fa772a61
VoThiKimTrang06101997 <vothikimtrang0610@gmail.com>
2023-01-20 02:33:50 +0700

    user photo upload function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/trang/ShopmeBackEnd_Admin/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/trang/ShopmeBackEnd_Admin/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.trang.ShopmeBackEnd_Admin.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.trang.ShopmeBackEnd_Admin.FileUploadUtil;
 import com.trang.ShopmeBackEnd_Admin.service.UserNotFoundException;
 import com.trang.ShopmeBackEnd_Admin.service.UserService;
 import com.trang.ShopmeCommon.entity.Role;
@@ -47,10 +52,28 @@ public class UserController {
 	@PostMapping("/users/save")
 	@ModelAttribute(value = "user")
 	// @RequestMapping(method = RequestMethod.POST)
-	public String saveUser(@ModelAttribute("user") User user, RedirectAttributes redirectAttributes) {
-		System.out.println(user);
+	public String saveUser(@ModelAttribute("user") User user, RedirectAttributes redirectAttributes,
+			@RequestParam("image") MultipartFile multipartFile ) throws IOException {
+		if(!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			User savedUser = userService.save(user);
+			
+			String uploadDir = "user-photos/" + savedUser.getId();
+			
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} else {
+			if(user.getPhotos().isEmpty()) {
+				user.setPhotos(null);
+				userService.save(user);
+			}
+		}
+		// System.out.println(user);
+		// System.out.println(multipartFile.getOriginalFilename());
+		
 		// model.addAttribute("user", user);
-		userService.save(user);
+		
 
 		redirectAttributes.addFlashAttribute("message", "The User had been saved successfully !");
 		return "redirect:/users";
@@ -88,7 +111,6 @@ public class UserController {
 	}
 
 	@GetMapping("/users/{id}/enabled/{status}")
-
 	public String updateUserEnabledStatus(@PathVariable("id") Integer id, @PathVariable("status") boolean enabled,
 			RedirectAttributes redirectAttributes) throws UserNotFoundException {
 
@@ -104,4 +126,6 @@ public class UserController {
 
 		return "redirect:/users";
 	}
+	
+	
 }
